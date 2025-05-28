86f9748aa84137c313481c0ca3f30c097dff5f44
GUTIERREZ Tom <tgz8009@solo.enseeiht.fr>
2025-05-28 14:02:37 +0200

    maj

---
+++ fr.n7.stl.minic/src/fr/n7/stl/minic/ast/instruction/Iteration.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package fr.n7.stl.minic.ast.instruction;
+
+import fr.n7.stl.minic.ast.Block;
+import fr.n7.stl.minic.ast.expression.Expression;
+import fr.n7.stl.minic.ast.instruction.declaration.FunctionDeclaration;
+import fr.n7.stl.minic.ast.scope.Declaration;
+import fr.n7.stl.minic.ast.scope.HierarchicalScope;
+import fr.n7.stl.minic.ast.type.AtomicType;
+import fr.n7.stl.tam.ast.Fragment;
+import fr.n7.stl.tam.ast.Register;
+import fr.n7.stl.tam.ast.TAMFactory;
+
+/**
+ * Implementation of the Abstract Syntax Tree node for a conditional instruction.
+ * @author Marc Pantel
+ *
+ */
+public class Iteration implements Instruction {
+
+	protected Expression condition;
+	protected Block body;
+
+	public Iteration(Expression _condition, Block _body) {
+		this.condition = _condition;
+		this.body = _body;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "while (" + this.condition + " )" + this.body;
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#collect(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean collectAndPartialResolve(HierarchicalScope<Declaration> _scope) {
+		return this.condition.collectAndPartialResolve(_scope) && this.body.collectAndPartialResolve(_scope);
+	}
+	
+	@Override
+	public boolean collectAndPartialResolve(HierarchicalScope<Declaration> _scope, FunctionDeclaration _container) {
+		return this.condition.collectAndPartialResolve(_scope) && this.body.collectAndPartialResolve(_scope,_container);
+	}
+	
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.instruction.Instruction#resolve(fr.n7.stl.block.ast.scope.Scope)
+	 */
+	@Override
+	public boolean completeResolve(HierarchicalScope<Declaration> _scope) {
+		return this.condition.completeResolve(_scope) && this.body.collectAndPartialResolve(_scope);
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#checkType()
+	 */
+	@Override
+	public boolean checkType() {
+		return this.condition.getType().equalsTo(AtomicType.BooleanType) && this.body.checkType();
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#allocateMemory(fr.n7.stl.tam.ast.Register, int)
+	 */
+	@Override
+	public int allocateMemory(Register _register, int _offset) {
+		body.allocateMemory(_register, _offset);
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see fr.n7.stl.block.ast.Instruction#getCode(fr.n7.stl.tam.ast.TAMFactory)
+	 */
+	@Override
+	public Fragment getCode(TAMFactory _factory) {
+		Fragment fragment = _factory.createFragment();
+		int LabelNumber = _factory.createLabelNumber();
+		fragment.append(condition.getCode(_factory));
+		fragment.addPrefix("BEGINWHILE"+LabelNumber);
+		//fragment.addComment(this.toString());
+		fragment.add(_factory.createJumpIf("ENDWHILE"+LabelNumber, 0));
+		fragment.append(body.getCode(_factory));
+		fragment.add(_factory.createJump("BEGINWHILE"+LabelNumber));
+		fragment.addSuffix("ENDWHILE"+LabelNumber);
+		return fragment;
+	}
+
+}
